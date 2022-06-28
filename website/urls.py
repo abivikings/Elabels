6eb442e8636b6c016452e0ada3fefaa5d29ddc45
Mustafizur Rahman <sojibvikings@gmail.com>
2022-06-28 15:02:47 +0600

    Digital one copy
    Digital one copy

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('', Home, name='home'),
     path('about_us/', AboutUs, name='about_us'),
     path('contact_us/', ContactUs, name='contact_us'),
+    path('app_suite/', AppSuite, name='app_suite'),
+    path('esl/', ESL, name='esl'),
 ]
